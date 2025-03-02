4e02bd96fa9fbe04eaa7472960dd4e6a8d7037ce
Ayush Kumar <fanduayush2000@gmail.com>
2025-03-02 06:49:53 +0530

    auth components

--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -15,11 +15,11 @@ export const Header = ({label}: HeaderProps) => {
     <div className="w-full flex flex-col text-clip gap-y-0 items-center justify-center">
       <h1
         className={cn(
-          "group flex items-center text-3xl font-semibold text-neutral-700 drop-shadow-md cursor-default",
+          "group flex items-start text-2xl font-semibold text-neutral-700 drop-shadow-md cursor-default",
           font.className
         )}
       >
-        Instant Stream
+        Globetrotter Challenge
       </h1>
       <p className="text-muted-foreground text-sm">{label}</p>
     </div>
--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import {Button} from "../ui/button";
-import {FcGoogle} from "react-icons/fc";
-import {FaGithub} from "react-icons/fa";
+// import {FcGoogle} from "react-icons/fc";
 import {signIn} from "next-auth/react";
 import {DEFAULT_LOGIN_REDIRECT} from "@/routes";
+import {Github} from "lucide-react";
 
 export const Social = () => {
   const onClick = (provider: "google" | "github") => {
@@ -16,7 +16,7 @@ export const Social = () => {
 
   return (
     <div className="flex items-center w-full gap-x-1">
-      <Button
+      {/* <Button
         onClick={() => onClick("google")}
         size={"lg"}
         variant={"outline"}
@@ -24,7 +24,7 @@ export const Social = () => {
       >
         Google
         <FcGoogle size={24} className="text-2xl" />
-      </Button>
+      </Button> */}
       <Button
         onClick={() => onClick("github")}
         size={"lg"}
@@ -32,7 +32,7 @@ export const Social = () => {
         className="w-full"
       >
         GitHub
-        <FaGithub size={24} className="text-2xl" />
+        <Github className="w-20 h-20" />
       </Button>
     </div>
   );
