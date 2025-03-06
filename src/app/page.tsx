88b1332c18ef9396f16c370083d0db618dfc4a5a
Ayush Kumar <fanduayush2000@gmail.com>
2025-03-06 20:22:45 +0530

    Landing page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,9 @@ import {Button} from "@/components/ui/button";
 import {auth, signOut} from "@/auth";
 import {cn} from "@/lib/utils";
 import Image from "next/image";
-import {LogIn, LogOut} from "lucide-react";
-// import {FaMapMarkedAlt} from "react-icons/fa";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import {LogIn} from "lucide-react";
 import Link from "next/link";
-
-// const font = Cherry_Bomb_One({
-//   subsets: ["latin"],
-//   weight: ["400"],
-// });
+import {NavDropdown} from "@/components/NarDropdown";
 
 const Headerfont = Oswald({
   subsets: ["cyrillic"],
@@ -44,110 +32,111 @@ const HomePage = async () => {
 
   return (
     <main className="overflow-auto overflow-x-hidden">
-      <section className="flex h-[100dvh] w-full bg-[#F8F8FF] scroll-smooth">
-        {/* <div className="radial-dot-background w-full fixed h-screen"></div> */}
-        <nav
-          className="fixed top-0 left-0 right-0 z-50 w-full h-20 flex justify-between 
-          items-center bg-[#1e1e1f] px-4 py-2
-          motion-translate-y-in-[-200%] motion-ease motion-duration-500"
-        >
-          <div className="flex items-center justify-center gap-2">
-            <Image src="/logo.svg" alt="Logo" width={50} height={50} />
-            <h1
-              className={cn(
-                "text-white/90 text-xl font-light",
-                fontRoboto.className
-              )}
-            >
-              Globetrotter
-            </h1>
-          </div>
-          <div className="mr-2">
-            {session ? (
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Image
-                    src={session?.user?.image || "/profile.svg"}
-                    width={50}
-                    height={50}
-                    alt={session?.user?.name || "Profile"}
-                    className="w-9 h-9 rounded-xl border-[2px] border-neutral-700 cursor-pointer bg-neutral-200"
-                  />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56 bg-neutral-900 text-zinc-300 border-px mr-6 mt-2">
-                  <DropdownMenuItem onClick={handleSignOut}>
-                    <LogOut />
-                    <span>Log out</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            ) : (
-              <Link href="/auth/login">
-                <Button
-                  className="motion-preset-focus-lg motion-duration-2000 group-hover text-neutral-950 bg-white-500 
+      <nav
+        className="fixed top-0 left-0 right-0 z-50 w-full h-20 flex justify-between 
+          items-center bg-white shadow-xl shadow-neutral-800/10 px-4 py-2
+          motion-translate-y-in-[-200%] motion-ease motion-duration-500 border-b-[0.1pt] border-black/40"
+      >
+        {/* Left - LOGO + NAME */}
+        <div className="flex items-center justify-center gap-2">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={0}
+            height={0}
+            className="rounded-full w-12 h-12 bg-zinc-100"
+          />
+          <h1
+            className={cn(
+              "text-neutral-600 text-xl font-normal",
+              fontRoboto.className
+            )}
+          >
+            Globetrotter
+          </h1>
+        </div>
+
+        {/* Right - DROPDOWN */}
+        <div className="mr-2">
+          {session ? (
+            <NavDropdown handleSignOut={handleSignOut} session={session} />
+          ) : (
+            <Link href="/auth/login">
+              <Button
+                className="motion-preset-focus-lg motion-duration-2000 group-hover text-neutral-950 bg-white-500 border-[0.11pt] border-black/20
                 rounded-lg px-4 py-2 hover:cursor-pointer shadow-lg hover:bg-white-500 hover:shadow-none
                 active:shadow-none active:bg-white-700 transition-all active:scale-[0.99]"
-                >
-                  <LogIn />
-                </Button>
-              </Link>
-            )}
-          </div>
-        </nav>
+              >
+                <LogIn />
+              </Button>
+            </Link>
+          )}
+        </div>
+      </nav>
 
-        <div className="w-full flex items-center justify-end p-6 relative">
+      <section className="landing_section flex">
+        {/* <div className="w-full relative">
           <Image
             src="/landing.svg"
             alt="landing"
-            width={900}
-            height={900}
-            className="mt-20 bg-yellow-300 shadow-lg shadow-black/10 border-[1pt] p-10 rounded-2xl"
+            width={0}
+            height={0}
+            className="absolute left-0  mt-20 md:w-[60%] md:opacity-100 opacity-10"
           />
+        </div> */}
 
-          <div className="absolute left-4 top-20 flex flex-col gap-10">
-            <h1
-              className={cn(
-                "header_bg font-bold text-[5rem] motion-preset-focus-lg -motion-translate-y-in-25 motion-duration-[1.5s]",
-                Headerfont.className
-              )}
-            >
-              Globetrotter
-            </h1>
+        <div className="absolute left-4 top-20 flex flex-col gap-10">
+          <h1
+            className={cn(
+              "header_bg font-bold text-[5rem] motion-preset-focus-lg -motion-translate-y-in-25 motion-duration-[1s]",
+              Headerfont.className
+            )}
+          >
+            Globetrotter
+          </h1>
 
-            <p
-              className={cn(
-                "text-md ml-2 motion-preset-focus-lg motion-delay-[0.5s] -motion-translate-y-in-25 motion-duration-[2s]",
-                fontLato.className
-              )}
-              style={{fontWeight: "0.1pt"}}
-            >
-              Guess the World, One Clue at a Time!
-              <br />
-              An interactive travel guessing game where users solve cryptic
-              clues,
-              <br />
-              unlock fun facts, and challenge friends to beat their scores!
-            </p>
+          <p
+            className={cn(
+              "text-md ml-2 motion-preset-focus-lg motion-delay-[0.3s] -motion-translate-y-in-25 motion-duration-[1.2s]",
+              fontLato.className
+            )}
+            style={{fontWeight: "0.1pt"}}
+          >
+            Guess the World, One Clue at a Time!
+            <br />
+            An interactive travel guessing game where users solve cryptic clues,
+            <br />
+            unlock fun facts, and challenge friends to beat their scores!
+          </p>
 
-            <Link href={"/game"}>
-              <Button
-                type="submit"
-                className="motion-preset-focus-lg border-spacing-4 border-[1pt] border-neutral-700 
-                w-[180px] h-[50px] motion-duration-2000 group-hover text-neutral-600 bg-transparent hover:bg-[#F8F8FF/10]
+          <Link href={"/game"}>
+            <Button
+              type="submit"
+              className="border-spacing-4 border-[1pt] border-neutral-700 
+                w-[180px] h-[50px] group-hover text-neutral-600 bg-transparent hover:bg-[#F8F8FF/10]
                 px-4 py-2 hover:cursor-pointer shadow-lg hover:bg-white-500 active:shadow-none 
                 shadow-white-400/30 active:bg-white-700 transition-all active:scale-[0.99] text-xl 
-                font-bold rounded-sm ml-2"
-              >
-                Play
-              </Button>
-            </Link>
-          </div>
+                font-bold rounded-sm ml-2 motion-preset-focus-lg motion-delay-[0.6s] -motion-translate-y-in-25 motion-duration-[1.2s]"
+            >
+              Play
+            </Button>
+          </Link>
         </div>
       </section>
-      <section className="w-full h-[100dvh] bg-[#F8F8FF]"></section>
-      <section className="w-full h-[100dvh] bg-[#F8F8FF]"></section>
+      <section className="landing_section"></section>
+      <section className="landing_section"></section>
     </main>
   );
 };
 
 export default HomePage;
+
+{
+  /* <Image
+                  src={session?.user?.image || "/profile.svg"}
+                  width={40}
+                  height={40}
+                  alt={session?.user?.name || "Profile"}
+                  className="rounded-full cursor-pointer bg-neutral-200"
+                /> */
+}
---
+++ src/components/NarDropdown.tsx
@@ -0,0 +1,155 @@
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {Button} from "./ui/button";
+import {Session} from "next-auth";
+
+type NavDropdownPros = {
+  session: Session | null;
+  handleSignOut: () => Promise<void>;
+};
+
+export function NavDropdown({handleSignOut, session}: NavDropdownPros) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="group" variant="outline" size="icon">
+          <svg
+            className="pointer-events-none"
+            width={16}
+            height={16}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 12L20 12"
+              className="origin-center -translate-y-[7px] transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+            />
+            <path
+              d="M4 12H20"
+              className="origin-center transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+            />
+            <path
+              d="M4 12H20"
+              className="origin-center translate-y-[7px] transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+            />
+          </svg>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-[100dvw] sm:w-56 mt-5">
+        <DropdownMenuLabel>{session?.user?.name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          {/* <DropdownMenuItem>
+            <User />
+            <span>Profile</span>
+          </DropdownMenuItem> */}
+
+          <DropdownMenuItem>
+            <CreditCard />
+            <span>Billing</span>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Settings />
+            <span>Settings</span>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>
+            <Keyboard />
+            <span>Keyboard shortcuts</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Users />
+            <span>Team</span>
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <UserPlus />
+              <span>Invite users</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>
+                  <Mail />
+                  <span>Email</span>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <MessageSquare />
+                  <span>Message</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem>
+                  <PlusCircle />
+                  <span>More...</span>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+
+          <DropdownMenuItem>
+            <Plus />
+            <span>New Team</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Github />
+          <span>GitHub</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <LifeBuoy />
+          <span>Support</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled>
+          <Cloud />
+          <span>API</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem onClick={handleSignOut}>
+          <LogOut />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
